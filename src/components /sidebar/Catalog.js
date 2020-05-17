f0d1a3f4783f62b8e9ff3b98dd7e608660c05179
ArthurDorokhin <a.u.dorokhin@gmail.com>
2020-05-17 19:07:34 +0300

    catalog

--- src/components /sidebar/Catalog.js
+++ src/components /sidebar/Catalog.js
@@ -1,40 +1,159 @@
 import React, {Component} from "react";
 import InfoProduct from "../infoProduct";
+import {catalog} from "../sidebar/constant"
+import Select from 'react-select';
+import Slider from 'rc-slider';
+import 'rc-slider/assets/index.css';
 
-export const Catalog = () => {
-    return (
-        <div className="info-page">
-            <div className="article-text">
-                <div className="sidebar-article">
-                    <ul className="sideMenu-list">
-                        <li className="sideMenu-i">
-                            <div className="sideMenu-t active"><a href="/catalog/">Каталог</a></div>
-                        </li>
-                        <li className="sideMenu-i">
-                            <div className="sideMenu-t"><a href="/o-nas/">О нас</a></div>
-                        </li>
-                        <li className="sideMenu-i">
-                            <div className="sideMenu-t"><a href="/oplata-i-dostavka/">Оплата и
-                                доставка</a></div>
-                        </li>
-                        <li className="sideMenu-i">
-                            <div className="sideMenu-t"><a href="/obmen-i-vozvrat/">Обмен и
-                                возврат</a></div>
-                        </li>
-                        <li className="sideMenu-i">
-                            <div className="sideMenu-t"><a href="/contacts/">Контактная
-                                информация</a></div>
-                        </li>
+const { createSliderWithTooltip } = Slider;
+const Range = createSliderWithTooltip(Slider.Range);
 
-                    </ul>
+const selectSort = [
+    {value: 0, label: 'по популярности'},
+    {value: 1, label: 'сначала дешевле'},
+    {value: 2, label: 'по названию'},
+];
+
+const selectCheckbox = [
+    {value: 0, label: "распродажа"},
+    {value: 1, label: "новинка"},
+    {value: 2, label: "хит"},
+];
+
+class Catalog extends Component {
+    state = {
+        valueSort: {label: this.props, value: this.props},
+        valueCheckbox: {name: this.props, value: this.props},
+        sliderValues: [9000, 15000],
+        activeRange: false
+    }
+
+    toggleClass = () => {
+        const active = this.state.activeRange;
+        this.setState({
+            activeRange: !active
+        })
+    };
+
+    handleChange = valueSort => {
+        this.setState({value: valueSort});
+        console.log(`Option selected:`, valueSort);
+    };
+
+
+    handleChangeCheckbox = valueCheckbox => {
+        this.setState({value: valueCheckbox});
+        console.log(`select checkbox:`, valueCheckbox);
+    };
+
+    handleChange124 = sliderValues => {
+        this.setState({ sliderValues });
+    };
+    render() {
+
+        const {selectedOption, selectCheck, sliderValues} = this.state;
+
+        return (
+            <div className="info-page">
+                <div className="article-text">
+                    <div className="main-content">
+                        <h1>Catalog</h1>
+                        <div className="sortPanel">
+                            <div className="filter">
+                                <div className="filter-title">
+                                    Фильтр
+                                </div>
+                                <div className="filterParams">
+                                    <div className="wrap-icon">
+                                        <div className="filter-icon">
+                                            Иконки
+                                        </div>
+                                        <div className="filter-iconList">
+                                            <Select
+                                                value={selectCheck}
+                                                onChange={this.handleChangeCheckbox}
+                                                options={selectCheckbox}
+                                            />
+                                        </div>
+                                    </div>
+                                    <div className={`${"wrap-price"} ${this.state.activeRange ? "active" : ""}`}>
+                                        <div className="filter-price-title"
+                                             onClick={this.toggleClass}>
+                                            Цены, грн
+                                            <span></span>
+                                        </div>
+                                    </div>
+                                    {/*{sliderValues[0]} - {sliderValues[1]}*/}
+                                    <div className="filter-price">
+
+                                        <Range
+                                            marks={{
+                                                7899: `грн 7899`,
+                                                23899: `грн 23899`
+                                            }}
+                                            min={7899}
+                                            max={23899}
+                                            defaultValue={sliderValues}
+                                            onChange={this.handleChange124}
+                                            tipFormatter={value => `${value}`}
+                                            tipProps={{
+                                                placement: "top",
+                                                visible: true
+                                            }}
+                                        />
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="sort">
+                                <div className="filter-title-sort">
+                                    Сортировка
+                                </div>
+                                <div className="sortList">
+                                    <Select
+                                        value={selectedOption}
+                                        onChange={this.handleChange}
+                                        options={selectSort}
+                                        defaultValue={{label: "по популярности", value: 0}}
+                                    />
+                                </div>
+                            </div>
+                        </div>
+                        <div className="catalog">
+                            {catalog.catalogItem.map((item) =>
+                                <div className="item-catalog" key={item.id}>
+                                    <div className="catalog-product">
+                                        <a href={item.link} className="link-product">
+                                            <div className="catalog-img">
+                                                <img
+                                                    src={item.img} alt={item.alt}/>
+                                            </div>
+                                        </a>
+                                        <div className="productSticker">
+                                            <div className="productView">
+                                                <div className="productSticker-item__promo"><p>РАСПРОДАЖА</p></div>
+                                                <div className="productSticker-item__new"><p>НОВИНКА</p></div>
+                                                <div className="productSticker-item__popular"><p>ХИТ</p></div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="catalogCard-info">
+                                        <div className="info-title">{item.info}</div>
+                                        <div className="btn-product">
+                                            <div className="info-price">{item.price}</div>
+                                            <div className="btn-buy">
+                                                <button>{item.buy}</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            )}
+                        </div>
+                    </div>
                 </div>
-                <div className="main-content">
-                    <h1>Catalog</h1>
-                </div>
+                <InfoProduct/>
             </div>
-            <InfoProduct/>
-        </div>
-    )
+        )
+    }
 };
 
 export default Catalog;
---
+++ src/components /sidebar/constant.js
@@ -0,0 +1,107 @@
+const catalog = {
+    catalogItem : [
+        {
+            id: 0,
+            link: "/elektrosamokat-sns-aluminium-65-duymov-black/",
+            img: "../img/content/elektrosamokat-sns-aluminium-65-duymov-black-91963412592561_small4.png",
+            alt: "Электровелосипед",
+            info: "Электровелосипед OIO CITY BIKE Elite White",
+            price: "22 990 грн",
+            buy: "Купить"
+        },
+        {
+            id: 1,
+            link: "/elektroskeyt-gtf-jetskate-classic-one-edition/",
+            img: "../img/content/elektroskeyt-gtf-jetskate-classic-one-edition-64377766348990_small4.png",
+            alt: "Электроскейт",
+            info: "Электроскейт Like.Bike Teens Skate",
+            price: "7 999 грн",
+            buy: "Купить"
+        },
+        {
+            id: 2,
+            link: "/elektrovelosiped-oio-city-bike-elite-white/",
+            img: "../img/content/elektrovelosiped-oio-city-bike-elite-white-97933455367029_small4.jpg",
+            alt: "Электровелосипед",
+            info: "Электровелосипед ZPAO SPORT’250",
+            price: "18 600 грн",
+            buy: "Купить"
+        },
+        {
+            id: 3,
+            link: "/monokoleso-inmotion-scv-v5f-black/",
+            img: "../img/content/monokoleso-inmotion-scv-v5f-black-76769264824427_small4.jpg",
+            alt: "Моноколесо",
+            info: "Моноколесо InMotion SCV V5F Black",
+            price: "15 999 грн",
+            buy: "Купить"
+        },
+        {
+            id: 4,
+            link: "/elektroskeyt-like.bike-teens-skate/",
+            img: "../img/content/elektroskeyt-like.bike-teens-skate-90979434112967_small4.jpg",
+            alt: "Электроскейт",
+            info: "Электроскейт Like.Bike Teens Skate",
+            price: "7 999 грн",
+            buy: "Купить"
+        },
+        {
+            id: 5,
+            link: "/elektroskeyt-gtf-jetskate-classic-two-edition/",
+            img: "../img/content/elektroskeyt-gtf-jetskate-classic-two-edition-92009107563403_small4.jpg",
+            alt: "Электроскейт",
+            info: "Электроскейт GTF Jetskate Classic Two Edition",
+            price: "10 999 грн",
+            buy: "Купить"
+        },
+        {
+            id: 6,
+            link: "/elektrosamokat-segway-ninebot-kickscooter-es2-gray/",
+            img: "../img/content/elektrosamokat-segway-ninebot-kickscooter-es2-gray-58413829178991_small4.jpg",
+            alt: "Электросамокат",
+            info: "Электросамокат Segway Ninebot KickScooter ES2 Gray",
+            price: "15 999 грн",
+            buy: "Купить"
+        },
+        {
+            id: 7,
+            link: "/elektrosamokat-sns-t9/",
+            img: "../img/content/elektrosamokat-sns-t9-12589246203178_small4.png",
+            alt: "Электросамокат",
+            info: "Электросамокат SNS T9",
+            price: "8 199 грн",
+            buy: "Купить"
+        },
+        {
+            id: 8,
+            link: "/monokoleso-segway-ninebot-a1-belyy/",
+            img: "../img/content/monokoleso-segway-ninebot-a1-belyy-31736205771711_small4.jpg",
+            alt: "Моноколесо",
+            info: "Моноколесо Segway Ninebot A1 Белый",
+            price: "12 999 грн",
+            buy: "Купить"
+        },
+        {
+            id: 9,
+            link: "/elektrovelosiped-oio-city-bike-elite-white/",
+            img: "../img/content/elektrovelosiped-zpao-sport250-20568225961503_small4.jpg",
+            alt: "Электровелосипед",
+            info: "Электровелосипед OIO CITY BIKE Elite White",
+            price: "22 990 грн",
+            buy: "Купить"
+        },
+        {
+            id: 10,
+            link: "/elektrovelosiped-skladnoy-kerambit-sport-bolids-36-500/",
+            img: "../img/content/elektrovelosiped-skladnoy-kerambit-sport-bolids-36-500-68989917908872_small4.png",
+            alt: "Электровелосипед",
+            info: "Электровелосипед складной Kerambit sport Bolids 36 500",
+            price: "23 899 грн",
+            buy: "Купить"
+        }
+    ]
+};
+
+export {
+    catalog
+}
